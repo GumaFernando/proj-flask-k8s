52bc3001ddffe55baf2021b08d421451fbfe23ce
Guma <guma_fernando@hotmail.com>
2022-07-28 09:38:01 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM  python:3.9.5
+RUN apt-get update -y && \
+  apt-get install -y python-pip python-dev
+
+COPY . /app
+
+RUN pip install flask
+
+WORKDIR /app
+
+EXPOSE 5000
+
+CMD ["python","app-fii.py"]
+
---
+++ app-fii.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, render_template,request
+
+
+app = Flask(__name__)
+
+@app.route("/funds")
+@app.route("/", methods=['GET', 'POST'])   # criando rota principal
+def bemvindo():
+    if request.method == 'POST':
+        FII_desejado  = request.form.get('FII_desejado')
+        #framework = request.form.get('framework')
+        return redirect(f'https://www.fundsexplorer.com.br/funds/{FII_desejado}')
+    return render_template('index.html')
+
+       
+if __name__  == "__main__":
+    app.run(debug=True, port=5000)  # colocando app para rodar na porta 3000
